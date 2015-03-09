53e890943fd3ede5fa5c6e336605e3c81e75ad25
huangliop <huangliop@163.com>
2015-03-09 09:51:33 +0800

    Update baidulocation.js

--- www/baidulocation.js
+++ www/baidulocation.js
@@ -2,7 +2,7 @@
    module.exports = {
        startLocation: function (success, error, options) {
            options = options || {};
-           var coorType = options.CoorType ? options.CoorType : 'bd0911';
+           var coorType = options.CoorType ? options.CoorType : 'bd09ll';
            var isNeedAddress = options.IsNeedAddress ? options.IsNeedAddress : false;
            var args = [coorType, isNeedAddress];
            exec(success, error, "BaiduLocation4Cordova", "startLocation", args);
